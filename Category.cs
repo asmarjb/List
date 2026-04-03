33183fc001c6b4c04960523b07a70b026ba5b53c
asmarjb <asmarjb@code.edu.az>
2026-04-03 23:16:17 +0400

    Add files via upload
    models

---
+++ Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listson.Helpers
+{
+    internal enum Category
+    {
+        Phone=1,
+        Laptop,
+        Tablet, 
+        Accessory
+    }
+}
